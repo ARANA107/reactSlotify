7efe2ff6f9899839fd69af5f5cbc4721a6246f1f
Ash Rana <ashrana1993@gmail.com>
2020-08-13 12:00:25 -0500

    Updates

---
+++ precache-manifest.49d42e4bbbe0270d92c896f612d356f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a52416aa6b1b3266d01c87541de4567",
+    "url": "/reactSlotify/index.html"
+  },
+  {
+    "revision": "3d2ec92d8ccf3078bf35",
+    "url": "/reactSlotify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5388dee09c2ede83dbf5",
+    "url": "/reactSlotify/static/js/2.bf0515a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactSlotify/static/js/2.bf0515a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2ec92d8ccf3078bf35",
+    "url": "/reactSlotify/static/js/main.97a7dd2b.chunk.js"
+  },
+  {
+    "revision": "49ae8a025dd15c7f6550",
+    "url": "/reactSlotify/static/js/runtime-main.cab29489.js"
+  }
+]);
