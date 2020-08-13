db98b888c336100d13dd17bdfa6472015fdb08c8
Ash Rana <ashrana1993@gmail.com>
2020-08-13 13:47:03 -0500

    rr

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,7 @@ class login extends Component {
   };
 
   render() {
-   const domain = "https://arana107.github.io/reactSlotify";
+    const imageUrl = process.env.PUBLIC_URL+ "/assets/images/ops.png";
     return (
         
       <div>
@@ -13,7 +13,7 @@ class login extends Component {
           <div className="wrap-login100 p-t-190 p-b-30">
             <form className="login100-form validate-form">
               <div className="login100-form-avatar">
-              <img src={"/assets/images/ops.png"} width="200px" height="200px"/>
+              <img src={imageUrl} width="200px" height="200px"/>
               </div>
 
               <span className="login100-form-title p-t-20 p-b-45">
